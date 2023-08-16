24f7f15f2c5a73c32a836a6e44910417eb9c564c
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-08-16 13:52:54 +0530

    done

--- src/components/Genesis.jsx
+++ src/components/Genesis.jsx
@@ -7,36 +7,36 @@ import img_2006 from '../assets/png/img_2006.png'
 import Image1 from '../assets/png/Image1.webp'
 import Image2 from '../assets/png/Image2.webp'
 import Image3 from '../assets/png/Image3.webp'
-import Image4 from '../assets/png/girl_img_2.webp'
-import Image5 from '../assets/png/Mcmanus.webp'
+import Image4 from '../assets/png/Image4.webp'
+import Image5 from '../assets/png/Image5.webp'
 import img_2007 from '../assets/png/img_2007.webp'
-import Image6 from '../assets/png/three_people_img.webp'
-import Image7 from '../assets/png/Arts.webp'
-import Image8 from '../assets/png/girl_img_3.webp'
-import Image9 from '../assets/png/exhibitions.webp'
+import Image6 from '../assets/png/Image6.webp'
+import Image7 from '../assets/png/Image7.webp'
+import Image8 from '../assets/png/Image8.webp'
+import Image9 from '../assets/png/Image9.webp'
 import img_2011_14 from '../assets/png/img_2011-14.webp'
-import Image10 from '../assets/png/girl_img_4.webp'
-import Image11 from '../assets/png/potts_point.webp'
-import Image12 from '../assets/png/Leon_gallery.webp'
+import Image10 from '../assets/png/Image10.webp'
+import Image11 from '../assets/png/Image11.webp'
+import Image12 from '../assets/png/Image12.webp'
 import Logo from '../assets/png/Logo.webp'
 import Para_2 from '../assets/png/Para_2.webp'
 import img_2005 from '../assets/png/img_2005.webp'
-import Image13 from '../assets/png/girl_img_5.webp'
-import Image14 from '../assets/png/girl_img_6.webp'
-import Image15 from '../assets/png/couple_img.webp'
-import Image16 from '../assets/png/soto_art.webp'
-import Image17 from '../assets/png/Style_news.webp'
+import Image13 from '../assets/png/Image13.webp'
+import Image14 from '../assets/png/Image14.webp'
+import Image15 from '../assets/png/Image15.webp'
+import Image16 from '../assets/png/Image16.webp'
+import Image17 from '../assets/png/Image17.webp'
 import img_2006_1 from '../assets/png/img_2006.webp'
-import Image18 from '../assets/png/girl_img_7.webp'
-import Image19 from '../assets/png/digital_divide.webp'
-import Image20 from '../assets/png/Leon_krasenstein.webp'
-import Image21 from '../assets/png/couple_img_2.webp'
+import Image18 from '../assets/png/Image18.webp'
+import Image19 from '../assets/png/Image19.webp'
+import Image20 from '../assets/png/Image20.webp'
+import Image21 from '../assets/png/Image21.webp'
 import img_2008 from '../assets/png/img_2008.webp'
 import img_2010 from '../assets/png/img_2010.webp'
-import Image22 from '../assets/png/couple_img_3.webp'
-import Image23 from '../assets/png/girl_img_8.webp'
-import Image24 from '../assets/png/girl_img_9.webp'
-import Image25 from '../assets/png/drawing_a_crowd.webp'
+import Image22 from '../assets/png/Image22.webp'
+import Image23 from '../assets/png/Image23.webp'
+import Image24 from '../assets/png/Image24.webp'
+import Image25 from '../assets/png/Image25.webp'
 import MyNavBar from './MyNavBar'
 
 const Genesis = () => {
@@ -67,7 +67,7 @@ const Genesis = () => {
                             <div className='Image3 pt-5'>
                                 <img className='w-100 h-100' src={Image3} alt="" />
                             </div>
-                            <div className='Image4 mx-3 py-5'>
+                            <div className='Image4 mx-4 py-5'>
                                 <img className='w-100' src={Image4} alt="" />
                             </div>
                             <div className='Image5 mx-4 pe-sm-4 py-5'>
@@ -85,11 +85,11 @@ const Genesis = () => {
                             <div className='Image8 pe-sm-5 pe-3'>
                                 <img className='w-100 h-100' src={Image8} alt="" />
                             </div>
-                            <div className='Image9 pt-sm-5 mt-5 pe-4 ps-sm-5 ms-sm-5'>
+                            <div className='Image9  mt-5 pe-4 ps-sm-5 ms-sm-5 pb-5'>
                                 <img className='w-100 h-100' src={Image9} alt="" />
                             </div>
-                            <div className='img_2011-14 pt-5 pb-sm-5 pe-5 pe-sm-0 ps-sm-5 ms-sm-5'>
-                                <img className='w-100 h-100' src={img_2011_14} alt="" />
+                            <div className='img_2011-14 pt-5 pb-sm-5 pe-5 pe-sm-0 ps-sm-5 ms-sm-5 mt-5'>
+                                <img className='w-100 h-100 pt-5' src={img_2011_14} alt="" />
                             </div>
                             <div className='Image10 py-4 ps-sm-5 pe-2 pe-sm-0'>
                                 <img className='w-100 h-100' src={Image10} alt="" />
@@ -120,7 +120,7 @@ const Genesis = () => {
                             <div className='Image15 ms-sm-4 pe-3 pt-5'>
                                 <img className='w-100 h-100' src={Image15} alt="" />
                             </div>
-                            <div className='Image16 ms-sm-4 pe-3 ps-1 pt-5'>
+                            <div className='Image16 ms-sm-4 pe-3  ps-3 pt-5'>
                                 <img className='w-100 h-100' src={Image16} alt="" />
                             </div>
                             <div className='Image17 ms-sm-4 pt-5 pe-3'>
@@ -133,7 +133,7 @@ const Genesis = () => {
                                 <img className='w-100' src={Image18} alt="" />
                             </div>
                             <div className='Image19 ps-sm-5 ms-sm-5 mt-4 pe-sm-2 pe-3'>
-                                <img className='w-100 h-100' src={Image19} alt="" />
+                                <img className='w-100 h-100 ps-3' src={Image19} alt="" />
                             </div>
                             <div className='Image20 pt-5 mx-sm-5 pe-3 pe-sm-0'>
                                 <img className='w-100 h-100' src={Image20} alt="" />
