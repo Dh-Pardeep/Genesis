a276db3c2df145972b4d2b363e51c1e09e396a52
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-08-16 14:13:12 +0530

    project complete

--- src/components/Genesis.jsx
+++ src/components/Genesis.jsx
@@ -1,168 +1,168 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import GenesisImg from '../assets/png/Genesis.webp'
-import Para_1 from '../assets/png/Para_1.webp'
-import img_2004 from '../assets/png/img_2004.webp'
-import img_2006 from '../assets/png/img_2006.png'
-import Image1 from '../assets/png/Image1.webp'
-import Image2 from '../assets/png/Image2.webp'
-import Image3 from '../assets/png/Image3.webp'
-import Image4 from '../assets/png/Image4.webp'
-import Image5 from '../assets/png/Image5.webp'
-import img_2007 from '../assets/png/img_2007.webp'
-import Image6 from '../assets/png/Image6.webp'
-import Image7 from '../assets/png/Image7.webp'
-import Image8 from '../assets/png/Image8.webp'
-import Image9 from '../assets/png/Image9.webp'
-import img_2011_14 from '../assets/png/img_2011-14.webp'
-import Image10 from '../assets/png/Image10.webp'
-import Image11 from '../assets/png/Image11.webp'
-import Image12 from '../assets/png/Image12.webp'
-import Logo from '../assets/png/Logo.webp'
-import Para_2 from '../assets/png/Para_2.webp'
-import img_2005 from '../assets/png/img_2005.webp'
-import Image13 from '../assets/png/Image13.webp'
-import Image14 from '../assets/png/Image14.webp'
-import Image15 from '../assets/png/Image15.webp'
-import Image16 from '../assets/png/Image16.webp'
-import Image17 from '../assets/png/Image17.webp'
-import img_2006_1 from '../assets/png/img_2006.webp'
-import Image18 from '../assets/png/Image18.webp'
-import Image19 from '../assets/png/Image19.webp'
-import Image20 from '../assets/png/Image20.webp'
-import Image21 from '../assets/png/Image21.webp'
-import img_2008 from '../assets/png/img_2008.webp'
-import img_2010 from '../assets/png/img_2010.webp'
-import Image22 from '../assets/png/Image22.webp'
-import Image23 from '../assets/png/Image23.webp'
-import Image24 from '../assets/png/Image24.webp'
-import Image25 from '../assets/png/Image25.webp'
+import GenesisImg from '../assets/image/png/Genesis.webp'
+import img_2004 from '../assets/image/png/img_2004.webp'
+import img_2006 from '../assets/image/png/img_2006.png'
+import Image1 from '../assets/image/png/Image1.webp'
+import Image2 from '../assets/image/png/Image2.webp'
+import Image3 from '../assets/image/png/Image3.webp'
+import Image4 from '../assets/image/png/Image4.webp'
+import Image5 from '../assets/image/png/Image5.webp'
+import img_2007 from '../assets/image/png/img_2007.webp'
+import Image6 from '../assets/image/png/Image6.webp'
+import Image7 from '../assets/image/png/Image7.webp'
+import Image8 from '../assets/image/png/Image8.webp'
+import Image9 from '../assets/image/png/Image9.webp'
+import img_2011_14 from '../assets/image/png/img_2011-14.webp'
+import Image10 from '../assets/image/png/Image10.webp'
+import Image11 from '../assets/image/png/Image11.webp'
+import Image12 from '../assets/image/png/Image12.webp'
+import Logo from '../assets/image/png/Logo.webp'
+import Para_2 from '../assets/image/png/Para_2.webp'
+import img_2005 from '../assets/image/png/img_2005.webp'
+import Image13 from '../assets/image/png/Image13.webp'
+import Image14 from '../assets/image/png/Image14.webp'
+import Image15 from '../assets/image/png/Image15.webp'
+import Image16 from '../assets/image/png/Image16.webp'
+import Image17 from '../assets/image/png/Image17.webp'
+import img_2006_1 from '../assets/image/png/img_2006.webp'
+import Image18 from '../assets/image/png/Image18.webp'
+import Image19 from '../assets/image/png/Image19.webp'
+import Image20 from '../assets/image/png/Image20.webp'
+import Image21 from '../assets/image/png/Image21.webp'
+import img_2008 from '../assets/image/png/img_2008.webp'
+import img_2010 from '../assets/image/png/img_2010.webp'
+import Image22 from '../assets/image/png/Image22.webp'
+import Image23 from '../assets/image/png/Image23.webp'
+import Image24 from '../assets/image/png/Image24.webp'
+import Image25 from '../assets/image/png/Image25.webp'
 import MyNavBar from './MyNavBar'
 
 const Genesis = () => {
     return (
         <div className='bg_color'>
-            <MyNavBar/>
+            <MyNavBar />
             <div className='overdflow-hidden py-5'>
-              <Container>
-              <Row className='py-md-5 mb_5 CenterLine'>
-                    <Col lg={6} className='pe-0 pb-5'>
-                        <div className='d-flex align-items-center justify-content-lg-end pt-3'>
-                            <img src={GenesisImg} alt="" />
-                        </div>
-                        <img className='pt-4 ps-2 ms-sm-5 pb-3' src={Para_1} alt="" />
-                        <div className='d_grid d-grid'>
-                            <div className='Image1 ms-5 pt-5 pe-3'>
-                                <img className='w-100 h-100' src={Image1} alt="" />
+                <Container>
+                    <Row className='py-md-5 mb_5 CenterLine'>
+                        <Col lg={6} className='pe-0 pb-5'>
+                            <div className='d-flex align-items-center justify-content-lg-end pt-3'>
+                                <img src={GenesisImg} alt="" />
                             </div>
-                            <div className=' img_2004 pt-4 ps-sm-4'>
-                                <img className='w-100' src={img_2004} alt="" />
+                            <p className='ff_Fontspring_light pt-4 ps-5 ms-sm-5 font_sm'>MANY YEARS BEFORE LAPiNC</p>
+                            <div className='d_grid d-grid'>
+                                <div className='Image1 ms-sm-5 pt-5 pe-3'>
+                                    <img className='w-100 h-100' src={Image1} alt="" />
+                                </div>
+                                <div className=' img_2004 pt-4 ps-sm-4'>
+                                    <img className='w-100' src={img_2004} alt="" />
+                                </div>
+                                <div className=' img_2006 pt-5 mt-3'>
+                                    <img className='w-100' src={img_2006} alt="" />
+                                </div>
+                                <div className=' Image2 ps-4 pe-4 mt-4 ps-lg-4'>
+                                    <img className='w-100 h-100' src={Image2} alt="" />
+                                </div>
+                                <div className='Image3 pt-5 ms-sm-5 ms-lg-0'>
+                                    <img className='w-100 h-100' src={Image3} alt="" />
+                                </div>
+                                <div className='Image4 mx-4 py-5'>
+                                    <img className='w-100' src={Image4} alt="" />
+                                </div>
+                                <div className='Image5 mx-4 pe-sm-4 py-5 ms-sm-5 ms-lg-0'>
+                                    <img className='w-100' src={Image5} alt="" />
+                                </div>
+                                <div className='img_2007'>
+                                    <img className='w-100' src={img_2007} alt="" />
+                                </div>
+                                <div className='Image6  ms-5 pe-3 py-5'>
+                                    <img className='w-100' src={Image6} alt="" />
+                                </div>
+                                <div className='Image7 pe-sm-5 pb-5 pb-sm-0 mx-5'>
+                                    <img className='w-100' src={Image7} alt="" />
+                                </div>
+                                <div className='Image8 pe-sm-5 pe-3'>
+                                    <img className='w-100 h-100' src={Image8} alt="" />
+                                </div>
+                                <div className='Image9  mt-5 pe-4 ps-sm-5 ms-sm-5 pb-5'>
+                                    <img className='w-100 h-100' src={Image9} alt="" />
+                                </div>
+                                <div className='img_2011-14 pt-5 pb-sm-5 pe-5 pe-sm-0 ps-sm-5 ms-sm-5 mt-5'>
+                                    <img className='w-100 h-100 pt-5' src={img_2011_14} alt="" />
+                                </div>
+                                <div className='Image10 py-4 ps-sm-5 pe-2 pe-sm-0'>
+                                    <img className='w-100 h-100' src={Image10} alt="" />
+                                </div>
+                                <div className='Image11 py-5  mt_5 pe-4 ps-4'>
+                                    <img className='w-100' src={Image11} alt="" />
+                                </div>
+                                <div className='Image12 ps-sm-5 pe-2 pe-sm-0'>
+                                    <img className='w-100 h-100' src={Image12} alt="" />
+                                </div>
                             </div>
-                            <div className=' img_2006 pt-5 mt-3'>
-                                <img className='w-100' src={img_2006} alt="" />
-                            </div>
-                            <div className=' Image2 ps-4 pe-4 mt-4'>
-                                <img className='w-100 h-100' src={Image2} alt="" />
-                            </div>
-                            <div className='Image3 pt-5'>
-                                <img className='w-100 h-100' src={Image3} alt="" />
-                            </div>
-                            <div className='Image4 mx-4 py-5'>
-                                <img className='w-100' src={Image4} alt="" />
-                            </div>
-                            <div className='Image5 mx-4 pe-sm-4 py-5'>
-                                <img className='w-100' src={Image5} alt="" />
-                            </div>
-                            <div className='img_2007'>
-                                <img className='w-100' src={img_2007} alt="" />
-                            </div>
-                            <div className='Image6  ms-5 pe-3 py-5'>
-                                <img className='w-100' src={Image6} alt="" />
-                            </div>
-                            <div className='Image7 pe-sm-5 pb-5 pb-sm-0 mx-5'>
-                                <img className='w-100' src={Image7} alt="" />
-                            </div>
-                            <div className='Image8 pe-sm-5 pe-3'>
-                                <img className='w-100 h-100' src={Image8} alt="" />
-                            </div>
-                            <div className='Image9  mt-5 pe-4 ps-sm-5 ms-sm-5 pb-5'>
-                                <img className='w-100 h-100' src={Image9} alt="" />
-                            </div>
-                            <div className='img_2011-14 pt-5 pb-sm-5 pe-5 pe-sm-0 ps-sm-5 ms-sm-5 mt-5'>
-                                <img className='w-100 h-100 pt-5' src={img_2011_14} alt="" />
-                            </div>
-                            <div className='Image10 py-4 ps-sm-5 pe-2 pe-sm-0'>
-                                <img className='w-100 h-100' src={Image10} alt="" />
-                            </div>
-                            <div className='Image11 py-5  mt_5 pe-4 ps-4'>
-                                <img className='w-100' src={Image11} alt="" />
-                            </div>
-                            <div className='Image12 ps-sm-5 pe-2 pe-sm-0'>
-                                <img className='w-100 h-100' src={Image12} alt="" />
-                            </div>
-                        </div>
-                    </Col>
+                        </Col>
 
-                    <Col lg={6}>
-                        <img className='pt-5 Logo' src={Logo} alt="Logo" />
-                        <img className='ps-sm-5 mx-sm-5 pt-5' src={Para_2} alt="" />
-
-                        <div className='d-grid d_grid'>
-                            <div className='img_2005 mx-sm-3 pt-5'>
-                                <img className='w-100' src={img_2005} alt="" />
+                        <Col lg={6}>
+                            <div className='d-flex pt-5 Logo align-items-center justify-content-between pe-5 me-5' >
+                                <img  src={Logo} alt="Logo" />
+                                <p className='ff_Fontspring_light font_sm para_2 text-center'>THIS ARTIST DUO WERE FORMALLY KNOWN AS LVSP</p>
                             </div>
-                            <div className='Image13 ms-sm-2 pt-5 pt-sm-0 pe-3 pe-sm-0'>
-                                <img className='w-100 h-100' src={Image13} alt="" />
+                            <div className='d-grid d_grid'>
+                                <div className='img_2005 mx-sm-3 pt-5'>
+                                    <img className='w-100' src={img_2005} alt="" />
+                                </div>
+                                <div className='Image13 ms-sm-2 pt-5 pt-sm-0 pe-3 pe-sm-0'>
+                                    <img className='w-100 h-100' src={Image13} alt="" />
+                                </div>
+                                <div className='Image14 ms-sm-5 pe-3 pe-sm-5 pt-sm-2 pt-5'>
+                                    <img className='w-100' src={Image14} alt="" />
+                                </div>
+                                <div className='Image15 ms-sm-4 pe-3 pt-5'>
+                                    <img className='w-100 h-100' src={Image15} alt="" />
+                                </div>
+                                <div className='Image16 ms-sm-4 pe-3  ps-3 pt-5'>
+                                    <img className='w-100 h-100' src={Image16} alt="" />
+                                </div>
+                                <div className='Image17 ms-sm-4 pt-5 pe-3'>
+                                    <img className='w-100 h-100' src={Image17} alt="" />
+                                </div>
+                                <div className='img_2006_1 ms-sm-3 pe-5 pe-sm-0 pt-5'>
+                                    <img className='w-100' src={img_2006_1} alt="" />
+                                </div>
+                                <div className='Image18 pt-sm-0 pt-5 mx-sm-5 pe-sm-5 pe-3'>
+                                    <img className='w-100' src={Image18} alt="" />
+                                </div>
+                                <div className='Image19 ps-sm-5 ms-sm-5 mt-4 pe-sm-2 pe-3'>
+                                    <img className='w-100 h-100 ps-3' src={Image19} alt="" />
+                                </div>
+                                <div className='Image20 pt-5 mx-sm-5 pe-3 pe-sm-0'>
+                                    <img className='w-100 h-100' src={Image20} alt="" />
+                                </div>
+                                <div className='Image21 pe-3 pe-sm-0 pt-5 ms-sm-5 ps-sm-4'>
+                                    <img className='w-100 h-100' src={Image21} alt="" />
+                                </div>
+                                <div className='img_2008 pt-5 mx-sm-3 pb-4 pe-5 pe-sm-0 pb-sm-0'>
+                                    <img className='w-100 h-100' src={img_2008} alt="" />
+                                </div>
+                                <div className='Image22 pt-4 pb-4 pe-3 pe-sm-0 pb-sm-0'>
+                                    <img className='w-100 h-100' src={Image22} alt="" />
+                                </div>
+                                <div className='img_2010 pt-4 mx-sm-3 pe-5 pe-sm-0 pb-4'>
+                                    <img className='w-100 h-100' src={img_2010} alt="" />
+                                </div>
+                                <div className='Image23 pt-5 ms-sm-5 pe-3'>
+                                    <img className='w-100 h-100' src={Image23} alt="" />
+                                </div>
+                                <div className='Image24 pe-3 pe-sm-0'>
+                                    <img className='w-100 h-100 mt-5' src={Image24} alt="" />
+                                </div>
+                                <div className='Image25 pt-sm-4 pt-5 mt-5 mt-sm-0 ms-sm-5 pe-3'>
+                                    <img className='w-100 h-100' src={Image25} alt="" />
+                                </div>
                             </div>
-                            <div className='Image14 ms-sm-5 pe-3 pe-sm-5 pt-sm-2 pt-5'>
-                                <img className='w-100' src={Image14} alt="" />
-                            </div>
-                            <div className='Image15 ms-sm-4 pe-3 pt-5'>
-                                <img className='w-100 h-100' src={Image15} alt="" />
-                            </div>
-                            <div className='Image16 ms-sm-4 pe-3  ps-3 pt-5'>
-                                <img className='w-100 h-100' src={Image16} alt="" />
-                            </div>
-                            <div className='Image17 ms-sm-4 pt-5 pe-3'>
-                                <img className='w-100 h-100' src={Image17} alt="" />
-                            </div>
-                            <div className='img_2006_1 ms-sm-3 pe-5 pe-sm-0 pt-5'>
-                                <img className='w-100' src={img_2006_1} alt="" />
-                            </div>
-                            <div className='Image18 pt-sm-0 pt-5 mx-sm-5 pe-sm-5 pe-3'>
-                                <img className='w-100' src={Image18} alt="" />
-                            </div>
-                            <div className='Image19 ps-sm-5 ms-sm-5 mt-4 pe-sm-2 pe-3'>
-                                <img className='w-100 h-100 ps-3' src={Image19} alt="" />
-                            </div>
-                            <div className='Image20 pt-5 mx-sm-5 pe-3 pe-sm-0'>
-                                <img className='w-100 h-100' src={Image20} alt="" />
-                            </div>
-                            <div className='Image21 pe-3 pe-sm-0 pt-5 ms-sm-5 ps-sm-4'>
-                                <img className='w-100 h-100' src={Image21} alt="" />
-                            </div>
-                            <div className='img_2008 pt-5 mx-sm-3 pb-4 pe-5 pe-sm-0 pb-sm-0'>
-                                <img className='w-100 h-100' src={img_2008} alt="" />
-                            </div>
-                            <div className='Image22 pt-4 pb-4 pe-3 pe-sm-0 pb-sm-0'>
-                                <img className='w-100 h-100' src={Image22} alt="" />
-                            </div>
-                            <div className='img_2010 pt-4 mx-sm-3 pe-5 pe-sm-0 pb-4'>
-                                <img className='w-100 h-100' src={img_2010} alt="" />
-                            </div>
-                            <div className='Image23 pt-5 ms-sm-5 pe-3'>
-                                <img className='w-100 h-100' src={Image23} alt="" />
-                            </div>
-                            <div className='Image24 pe-3 pe-sm-0'>
-                                <img className='w-100 h-100 mt-5' src={Image24} alt="" />
-                            </div>
-                            <div className='Image25 pt-sm-4 pt-5 mt-5 mt-sm-0 ms-sm-5 pe-3'>
-                                <img className='w-100 h-100' src={Image25} alt="" />
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
-              </Container>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
 
         </div >
